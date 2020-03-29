2bb0f26fa6ba485b84bfafdf85a0ca49b833d947
Dominik Bucko <dominik.bucko@external.pan-net.eu>
2020-03-29 17:29:58 +0200

    login adjustments

--- src/main/java/com/icloud/dominik/UI/Home.java
+++ src/main/java/com/icloud/dominik/UI/Home.java
@@ -1,14 +1,14 @@
 package com.icloud.dominik.UI;
 
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route("home")
+@Route("")
 public class Home extends VerticalLayout {
     public Home() {
-        Text welcome = new Text("WELCOME");
-        VerticalLayout layout = new VerticalLayout(welcome);
+        VerticalLayout layout = new VerticalLayout();
         layout.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         add(layout);
     }
--- src/main/java/com/icloud/dominik/UI/LoginPage.java
+++ src/main/java/com/icloud/dominik/UI/LoginPage.java
@@ -1,48 +1,42 @@
 package com.icloud.dominik.UI;
 
-import backend.authentication.Authenticator;
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.charts.model.TextAlign;
-import com.vaadin.flow.component.charts.model.Title;
-import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.PasswordField;
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.PWA;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
-import java.util.List;
-@Route("")
-public class LoginPage extends VerticalLayout {
-    public LoginPage() {
-        Text title = new Text("Smart Asset Management - SAM");
-        TextField username = new TextField("Username");
-        PasswordField password = new PasswordField("Password");
-        Button button = new Button("Login");
-        VerticalLayout layout = new VerticalLayout(title, username, password, button);
-        layout.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        add(layout);
+import java.util.Collections;
 
-        button.addClickListener(click -> logIn(username.getValue(), password.getValue(), button, layout));
-    }
+@Route("login")
+@PageTitle("Login | SAM")
 
-    private void logIn(String username, String password, Button button, VerticalLayout layout) {
-        Authenticator auth = new Authenticator(username, password);
-        if (auth.authenticate()){
-            button.getUI().ifPresent(ui -> ui.navigate("home"));
-        }
-        else {
-            Notification notification = new Notification("Wrong credentials", 3000);
-            notification.open();
-        }
-    }
+public class LoginPage extends VerticalLayout implements BeforeEnterObserver {
 
+	private LoginForm login = new LoginForm();
+
+	public LoginPage(){
+		addClassName("login-view");
+		setSizeFull();
+		setAlignItems(Alignment.CENTER);
+		setJustifyContentMode(JustifyContentMode.CENTER);
+
+		login.setAction("login");
+
+		add(new H1("SAM - Smart Asset Manager"), login);
+	}
+
+	@Override
+	public void beforeEnter(BeforeEnterEvent event) {
+		// inform the user about an authentication error
+		if(!event.getLocation()
+			.getQueryParameters()
+			.getParameters()
+			.getOrDefault("error", Collections.emptyList())
+			.isEmpty()) {
+			login.setError(true);
+		}
+	}
 }
